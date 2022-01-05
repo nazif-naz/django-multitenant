957b0814e0f12c44d733a2e60436db91906be292
MaziyarMK <maziyarmalakouti@gmail.com>
2022-01-05 20:35:16 +0400

    disable the changes for the time being

--- django_multitenant/backends/postgresql/base.py
+++ django_multitenant/backends/postgresql/base.py
@@ -138,36 +138,37 @@ class DatabaseFeatures(PostgresqlDatabaseFeatures):
 
 
 class DatabaseOperations(PostgresqlDatabaseOperations):
-    def sequence_reset_sql(self, style, model_list):
-        from django.db import models
-        output = []
-        qn = self.quote_name
-        for model in model_list:
-            # Use `coalesce` to set the sequence for each model to the max pk value if there are records,
-            # or 1 if there are none. Set the `is_called` property (the third argument to `setval`) to true
-            # if there are records (as the max pk value is already in use), otherwise set it to false.
-            # Use pg_get_serial_sequence to get the underlying sequence name from the table name
-            # and column name (available since PostgreSQL 8)
-
-            for f in model._meta.local_fields:
-                if isinstance(f, models.AutoField):
-                    # START CHANGES
-                    output.append(
-                        "%s setval(pg_get_serial_sequence('%s','%s'), "
-                        "coalesce(max(%s), 1), max(%s) %s null);" % (
-                            style.SQL_KEYWORD('SELECT'),
-                            style.SQL_TABLE(qn(model._meta.db_table)),
-                            style.SQL_FIELD(f.column),
-                            style.SQL_FIELD(qn(f"{model._meta.db_table}.{f.column}")),
-                            style.SQL_FIELD(qn(f"{model._meta.db_table}.{f.column}")),
-                            style.SQL_KEYWORD('IS NOT'),
-                            # style.SQL_KEYWORD('FROM'),
-                            # style.SQL_TABLE(qn(model._meta.db_table)),
-                        )
-                    )
-                    # END CHANGES
-                    break  # Only one AutoField is allowed per model, so don't bother continuing.
-        return output
+    pass
+    # def sequence_reset_sql(self, style, model_list):
+    #     from django.db import models
+    #     output = []
+    #     qn = self.quote_name
+    #     for model in model_list:
+    #         # Use `coalesce` to set the sequence for each model to the max pk value if there are records,
+    #         # or 1 if there are none. Set the `is_called` property (the third argument to `setval`) to true
+    #         # if there are records (as the max pk value is already in use), otherwise set it to false.
+    #         # Use pg_get_serial_sequence to get the underlying sequence name from the table name
+    #         # and column name (available since PostgreSQL 8)
+    #
+    #         for f in model._meta.local_fields:
+    #             if isinstance(f, models.AutoField):
+    #                 # START CHANGES
+    #                 output.append(
+    #                     "%s setval(pg_get_serial_sequence('%s','%s'), "
+    #                     "coalesce(max(%s), 1), max(%s) %s null);" % (
+    #                         style.SQL_KEYWORD('SELECT'),
+    #                         style.SQL_TABLE(qn(model._meta.db_table)),
+    #                         style.SQL_FIELD(f.column),
+    #                         style.SQL_FIELD(qn(f.column)),
+    #                         style.SQL_FIELD(qn(f.column)),
+    #                         style.SQL_KEYWORD('IS NOT'),
+    #                         style.SQL_KEYWORD('FROM'),
+    #                         style.SQL_TABLE(qn(model._meta.db_table)),
+    #                     )
+    #                 )
+    #                 # END CHANGES
+    #                 break  # Only one AutoField is allowed per model, so don't bother continuing.
+    #     return output
 
 
 class DatabaseWrapper(PostgresqlDatabaseWrapper):
